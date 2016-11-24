570a7dc94cea2c81ebbb302c2484bbb7dd6d0c02
Jordi Prats <jprats@systemadmin.es>
2016-11-24 13:18:53 +0100

    afegit include

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 0.1.5
+
+* included multipathd when trying to push any alias
+
 ## 0.1.0
 
 initial release
--- manifests/alias.pp
+++ manifests/alias.pp
@@ -1,5 +1,7 @@
 define multipathd::alias($wwid, $aliasname=$name) {
 
+  include ::multipathd
+
   concat::fragment { "alias ${wwid}":
     target  => '/etc/multipath.conf',
     order   => '11',
